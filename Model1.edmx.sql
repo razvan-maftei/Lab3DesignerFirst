e56b624f107eef87e3399af0a009eab4e8657400
RasterDX <rasterplane@hotmail.com>
2020-03-23 10:24:56 -0700

    Add project files.

---
+++ Model1.edmx.sql
@@ -0,0 +1,176 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/23/2020 10:21:59
+-- Generated from EDMX file: C:\Users\raste\source\repos\EFModelDesignerFirst\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [test];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_CustomerOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_CustomerOrder];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[People]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[People];
+GO
+IF OBJECT_ID(N'[dbo].[Customers]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customers];
+GO
+IF OBJECT_ID(N'[dbo].[Orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Orders];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'People'
+CREATE TABLE [dbo].[People] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(10)  NOT NULL,
+    [LastName] nvarchar(10)  NOT NULL,
+    [MiddleName] nvarchar(10)  NOT NULL,
+    [TelephoneNumber] nvarchar(10)  NOT NULL
+);
+GO
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [City] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [TotalValue] nvarchar(max)  NOT NULL,
+    [Date] nvarchar(max)  NOT NULL,
+    [CustomerId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Albums'
+CREATE TABLE [dbo].[Albums] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Artists'
+CREATE TABLE [dbo].[Artists] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'AlbumArtist'
+CREATE TABLE [dbo].[AlbumArtist] (
+    [Albums_Id] int  NOT NULL,
+    [Artists_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'People'
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [PK_People]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Albums'
+ALTER TABLE [dbo].[Albums]
+ADD CONSTRAINT [PK_Albums]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Artists'
+ALTER TABLE [dbo].[Artists]
+ADD CONSTRAINT [PK_Artists]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Albums_Id], [Artists_Id] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [PK_AlbumArtist]
+    PRIMARY KEY CLUSTERED ([Albums_Id], [Artists_Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [CustomerId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK_CustomerOrder]
+    FOREIGN KEY ([CustomerId])
+    REFERENCES [dbo].[Customers]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CustomerOrder'
+CREATE INDEX [IX_FK_CustomerOrder]
+ON [dbo].[Orders]
+    ([CustomerId]);
+GO
+
+-- Creating foreign key on [Albums_Id] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [FK_AlbumArtist_Album]
+    FOREIGN KEY ([Albums_Id])
+    REFERENCES [dbo].[Albums]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Artists_Id] in table 'AlbumArtist'
+ALTER TABLE [dbo].[AlbumArtist]
+ADD CONSTRAINT [FK_AlbumArtist_Artist]
+    FOREIGN KEY ([Artists_Id])
+    REFERENCES [dbo].[Artists]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_AlbumArtist_Artist'
+CREATE INDEX [IX_FK_AlbumArtist_Artist]
+ON [dbo].[AlbumArtist]
+    ([Artists_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EFModelDesignerFirst {
+    internal class Program {
+        private static void Main()
+        {
+            TestPerson();
+            Console.WriteLine("Press any key to exit: ");
+            Console.ReadLine();
+        }
+
+        private static void TestPerson()
+        {
+            using (var context = new Model1Container())
+            {
+                var p = new Person()
+                {
+                    FirstName = "Julie",
+                    LastName = "Andrew",
+                    MiddleName = "T",
+                    TelephoneNumber = "09876"
+                };
+                context.People.Add(p);
+                context.SaveChanges();
+                var items = context.People;
+                foreach (var x in items)
+                    Console.WriteLine("{0} {1}", x.Id, x.FirstName);
+            }
+
+            ;
+        }
+
+        private static void TestOneToMany()
+        {
+            Console.WriteLine("One to many association");
+            using (var context = new Model1Container())
+            {
+                var c = new Customer()
+                {
+                    Name = "Customer 1",
+                    City = "Iasi"
+                };
+                var o1 = new Order()
+                {
+                    TotalValue = 200.ToString(),
+                    Date = DateTime.Now.ToString(),
+                    Customer = c
+                };
+                var o2 = new Order()
+                {
+                    TotalValue = 300.ToString(),
+                    Date = DateTime.Now.ToString(),
+                    Customer = c
+                };
+                context.Customers.Add(c);
+                context.Orders.Add(o1);
+                context.Orders.Add(o2);
+                context.SaveChanges();
+                var items = context.Customers;
+                foreach (var x in items)
+                {
+                    Console.WriteLine("Customer : {0}, {1}, {2}", x.Id, x.Name, x.City);
+                    foreach (var ox in x.Orders)
+                        Console.WriteLine("\tOrders: {0}, {1}, {2}", ox.Id, ox.Date, ox.TotalValue);
+                }
+            }
+        }
+
+    }
+}
